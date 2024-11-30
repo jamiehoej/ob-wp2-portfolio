ddea0caf128ee7ba2e97f7886414793e118e03a8
jamiehoej <jamiehoej@gmail.com>
2024-11-30 20:23:20 +0100

    update backend eslint.config.js

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,15 +1,15 @@
 import js from "@eslint/js";
+import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   js.configs.recommended,
   {
+    files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-      // Your custom rules
+      globals: {
+        ...globals.node,
+      },
     },
   },
   eslintConfigPrettier,
